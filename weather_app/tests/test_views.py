44ed3004e9d5864135b5616577072bf05379abe9
Sana451 <sana451@mail.ru>
2024-07-18 22:22:25 +0300

    Модульный тест: Протестирована корректная работа представлений (отображение корректных шаблонов, а также возвращаемый от АПИ прогноз погоды) посредством тестового клиента (django.test.Client).

---
+++ weather_app/tests/test_views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+
+from django.test import Client
+from pytest_django.asserts import assertTemplateUsed
+
+
+def test_home_page_returns_correct_html(client: Client):
+    """Тест: домашняя страница возвращает правильный html"""
+    response = client.get("/")
+    assertTemplateUsed(response, "home.html")
+
+
+class TestForecastView:
+    """Тест отображения прогноза погоды"""
+
+    def test_uses_weather_template(self, client: Client):
+        """Тест: используется шаблон прогноза погоды."""
+        response = client.post("/api/", data={"city": "Санкт-Петербург"})
+        assertTemplateUsed(response, "weather_forecast.html")
+
+    def test_returns_correct_json(self, client: Client):
+        """Тест: прогноз погоды содержится в контексте ответа."""
+        response: HttpResponse = client.post("/api/", data={"city": "Санкт-Петербург"})
+        assert response.context["city"] == "Санкт-Петербург"
+        assert "Атмосферное давление" in response.context["current"]
+        assert "Температура" in response.context["hourly"]
